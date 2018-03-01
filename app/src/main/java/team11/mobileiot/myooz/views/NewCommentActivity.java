ebb5bc654a8de3fa159830d34fffb3396bf90ec7
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-03-01 00:17:58 -0500

    Add New Comment Activity.

---
+++ app/src/main/java/team11/mobileiot/myooz/views/NewCommentActivity.java
@@ -0,0 +1,15 @@
+package team11.mobileiot.myooz.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import team11.mobileiot.myooz.R;
+
+public class NewCommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_comment);
+    }
+}
